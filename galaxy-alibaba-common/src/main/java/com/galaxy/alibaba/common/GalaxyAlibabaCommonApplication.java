7afcaf47bd81f0d8c81ef44e41970380f00b7207
sky123shilin <sky123shilin@outlook.com>
2023-02-17 09:22:33 +0800

    第一次提交，初始化项目

---
+++ galaxy-alibaba-common/src/main/java/com/galaxy/alibaba/common/GalaxyAlibabaCommonApplication.java
@@ -0,0 +1,13 @@
+package com.galaxy.alibaba.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalaxyAlibabaCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalaxyAlibabaCommonApplication.class, args);
+	}
+
+}
---
+++ galaxy-alibaba-common/src/test/java/com/galaxy/alibaba/common/GalaxyAlibabaCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galaxy.alibaba.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalaxyAlibabaCommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
